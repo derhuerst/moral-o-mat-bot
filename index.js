8540a5241358a06dba5188180d6fe03349176a19
Jannis R <mail@jannisr.de>
2018-07-21 15:50:33 +0200

    rewrite from eschen-bot

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict'
 
-const path = require('path')
-const level = require('level')
-const setupTimeouts = require('./lib/timeouts')
-const floor = require('floordate')
-const Bot = require('node-telegram-bot-api')
+const Bot = require('telegraf')
+const escape = require('js-string-escape')
 const moralOMat = require('moral-o-mat')
+const through = require('through2')
+const level = require('level')
+const floor = require('floordate')
+const {format: formatUrl} = require('url')
 
-const DB = process.env.DB || path.join(__dirname, 'db')
+const watching = require('./lib/watching')
+const on8am = require('./lib/on-8-am')
 
 const TOKEN = process.env.TOKEN
 if (!TOKEN) {
@@ -15,39 +17,113 @@ if (!TOKEN) {
 	process.exit(1)
 }
 
-const hour = 60 * 60 * 1000
-const day = 24 * hour
-
-const sendStatement = (user) => {
-	timeouts.put(+tomorrow8am(), user, (err) => {
-		if (err) console.error(err)
-	})
-	bot.sendMessage(user, 'Guten Morgen! ' + moralOMat())
+const logErr = (err) => {
+	if (process.env.NODE_ENV === 'dev') console.error(err)
+	else console.error(err && err.message || (err + ''))
 }
 
-const db = level(DB)
-const timeouts = setupTimeouts(db, sendStatement)
+const helpMsg = `\
+\`/moral\` – Schickt eine einzelne Moral.
+\`/start\` – Wird dir jeden Morgen eine Moral schicken.
+\`/stop\` – Damit aufhören.`
+const errMsg = `\
+Shit! Irgendwas stimmt hier nicht. Bitte probier das noch mal.`
 
-const tomorrow8am = () => new Date(+floor(new Date(), 'day') + day + 8 * hour)
+const bot = new Bot(TOKEN)
+bot.telegram.getMe().then(({username}) => {
+	bot.options.username = username
+})
 
-const bot = new Bot(TOKEN, {polling: true})
+bot.use(async (ctx, next) => {
+	if (!ctx.chat || !ctx.chat.id) return null
+	ctx.user = ctx.chat.id
 
-bot.on('message', (msg) => {
-	if (!msg.text) return
-	const chat = msg.chat.id
-	const text = msg.text.trim()
+	const t0 = Date.now()
+	await next(ctx)
+	const d = Date.now() - t0
 
-	if (text === '/stop') {
-		timeouts.del(chat, (err) => {
-			if (err) bot.sendMessage(chat, 'Oops! ' + err.message)
-			else bot.sendMessage(chat, 'Okay, Message angekommen…')
-		})
-	} else if (text === '/start') {
-		timeouts.put(chat, +tomorrow8am(), (err) => {
-			if (err) bot.sendMessage(chat, 'Oops! ' + err.message)
-			else bot.sendMessage(chat, 'Okay, werde dir morgen um 8 was schicken.')
-		})
-	} else if (text === '/moral') {
-		bot.sendMessage(chat, moralOMat())
+	const msg = ctx.message
+	console.info([
+		d + 'ms',
+		msg && msg.date || '[unknown date]',
+		ctx.user,
+		msg && msg.text && escape(msg.text.slice(0, 30)) || '[no message]'
+	].join(' '))
+})
+
+bot.command('/moral', ctx => ctx.reply(moralOMat()))
+
+bot.command('/start', async (ctx) => {
+	try {
+		await watching.add(ctx.user)
+		await ctx.reply('Okay, werde dir morgen um 8 was schicken.')
+	} catch (err) {
+		logErr(err)
+		await ctx.reply(errMsg)
 	}
 })
+bot.command('/stop', async (ctx) => {
+	try {
+		await watching.del(ctx.user)
+		await ctx.reply('Okay, Message angekommen…')
+	} catch (err) {
+		logErr(err)
+		await ctx.reply(errMsg)
+	}
+})
+
+bot.use(ctx => ctx.replyWithMarkdown(helpMsg))
+
+on8am(() => {
+	const msg = moralOMat()
+	let receivers = 0
+
+	watching.all()
+	.pipe(through.obj((user, _, cb) => {
+		receivers++
+		bot.telegram.sendMessage(user, msg)
+		.then(() => cb())
+		.catch(cb)
+	}))
+	.on('data', () => {})
+	.once('finish', () => {
+		console.info(`Notified ${receivers} people about ${beer.name}.`)
+	})
+	.once('error', logErr)
+})
+
+if (process.env.NODE_ENV === 'dev') {
+	console.info('using polling')
+
+	bot.telegram.deleteWebhook()
+	.then(() => bot.startPolling())
+	.catch(logErr)
+} else {
+	console.info('using web hook')
+
+	const WEB_HOOK_HOST = process.env.WEB_HOOK_HOST
+	if (!WEB_HOOK_HOST) {
+		console.error('Missing WEB_HOOK_HOST env var.')
+		process.exit(1)
+	}
+	const WEB_HOOK_PATH = process.env.WEB_HOOK_PATH
+	if (!WEB_HOOK_PATH) {
+		console.error('Missing WEB_HOOK_PATH env var.')
+		process.exit(1)
+	}
+	const WEB_HOOK_PORT = process.env.WEB_HOOK_PORT && parseInt(process.env.WEB_HOOK_PORT)
+	if (!WEB_HOOK_PORT) {
+		console.error('Missing WEB_HOOK_PORT env var.')
+		process.exit(1)
+	}
+
+	bot.webhookReply = false
+	bot.telegram.setWebhook(formatUrl({
+		protocol: 'https',
+		host: WEB_HOOK_HOST,
+		pathname: WEB_HOOK_PATH
+	}))
+	bot.startWebhook(WEB_HOOK_PATH, null, WEB_HOOK_PORT)
+}
+
+bot.catch(console.error)
